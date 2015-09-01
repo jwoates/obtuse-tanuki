e43183fac58ef94db6b1f518455138e79141f9c1
Foad Mozaffari <foad.mozaffari@modahealth.com>
2015-09-01 14:07:15 -0700

    sorting test WIP

--- card-shuffle-test/cards.js
+++ card-shuffle-test/cards.js
@@ -1,14 +1,13 @@
-var deck = function(){
-  this.numbers = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2' ],
-    this.suits = ['spade', 'diamond', 'heart', 'club'],
+Deck = function(){
+    this.numbers = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2' ];
+    this.suits = ['spade', 'diamond', 'heart', 'club'];
+    this.sortedDeck = [];
     this.sort = function(){
-          for(i=0; i <this.suits.length; i++){
-                      for(j=0; j < this.numbers.length; j++){
-                                       return (this.numbers[j] + " " + this.suits[i]);
-                      
-                                  }
-                  }
+        for(i=0; i <this.suits.length; i++){
+            for(j=0; j < this.numbers.length; j++){
+                this.sortedDeck.push(this.numbers[j] + " " + this.suits[i])
+            }
         }
+        return this.sortedDeck;
+    }
 }
-var newDeck = new deck;
-newDeck.sort();
