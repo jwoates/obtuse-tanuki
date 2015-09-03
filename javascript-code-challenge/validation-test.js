e4b74349d73ddc4b850e36c49267db63b2db46d5
Foad Mozaffari <foad.mozaffari@modahealth.com>
2015-09-03 15:20:48 -0700

    final validation with green test

--- javascript-code-challenge/validation-test.js
+++ javascript-code-challenge/validation-test.js
@@ -6,7 +6,8 @@ var code = fs.readFileSync(path);
 vm.runInThisContext(code);
 
 describe('validation function', function(){
-    it('validates the input', function(){
-        assert.equal(validation('Ze'), 'Ze VALID');
+    it('validates the first string', function(){
+        assert.deepEqual(validation('Ze Za Nj QZja\nRhfa QRZZaZZaj'), ["Ze VALID", "Za VALID", "Nj INVALID", "QZja VALID", "Rhfa VALID", "QRZZaZZaj VALID"]);
     })
 });
+
--- javascript-code-challenge/validation.js
+++ javascript-code-challenge/validation.js
@@ -1,16 +1,16 @@
 var regex = /(^[a-j]{1}$)|(^[NLQR]{1}$)|(^[NLQR](?=([a-j]|[ZNLQR]){2}))|(^[Z](?:_?[a-j]+)*$)/;
-var splitWith =/ +?/g;
+var splitWith =/[\n ]/;
 var validation = function(inputSting){
+    var results = [];
     var string = inputSting.split(splitWith);
     for(i=0; i < string.length; i++){
         var valid = regex.test(string[i]);
         if(valid){
-            return (string[i] + " VALID");
-            console.log(string[i] + " VALID");
+            results.push(string[i] + " VALID");
         }else{
-            console.log(string[i] + " INVALID");
-            return (string[i] + " INVALID")
+            results.push(string[i] + " INVALID");
         };
     }
+    return results;
 };
-//validation('Ze Za Nj QZja\nRhfa QRZZaZZaj ssfsfsf');
+validation('Ze Za Nj QZja\nRhfa QRZZaZZaj');
